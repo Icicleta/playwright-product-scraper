0e4fcbc6ce5f494c4e383b350a7ea2d9b6b6ae0f
Icicleta <novofernandeziciar@gmail.com>
2026-03-18 22:29:20 +0100

    feat: relocate scraper code into crawler module

--- scraper.py
+++ scraper.py
@@ -1,21 +1,14 @@
 from playwright.sync_api import sync_playwright
 
+def get_page_content(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
 
-with sync_playwright() as pw:
-    browser = pw.chromium.launch()
-    page = browser.new_page()
+        page.goto(url)
 
-    page.goto("https://books.toscrape.com")
-    page.wait_for_selector(".page_inner")
+        html = page.content()
 
-    books = page.query_selector_all(".product_pod")
+        browser.close()
 
-    for book in books:
-        title = book.query_selector("h3 a").inner_text().strip()
-        price = book.query_selector(".product_price .price_color").inner_text()
-        stock = bool(book.query_selector(".product_price .instock"))
-        image = book.query_selector(".image_container img").get_attribute("src")
-
-        print(f"Book Title: {title}\n Book Price: {price} \n Book Stock: {stock} \n Book Image: {image}")
-
-    browser.close()
+        return html
