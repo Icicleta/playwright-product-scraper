64a2a07f3d4eed18401f01d021ff5ce11aee422a
Icicleta <novofernandeziciar@gmail.com>
2026-03-19 13:31:01 +0100

    feat: add csv export functionality

---
+++ scraper/exporter.py
@@ -0,0 +1,7 @@
+import csv
+
+def export_to_csv(data, filename):
+    with open(filename, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
